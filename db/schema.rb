d04e1b03f06b1f35ebcffcca9591eae2119dd70e
Brit Butler <redline6561@gmail.com>
2015-10-26 10:50:05 -0400

    Add PostTag model and associations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026143711) do
+ActiveRecord::Schema.define(version: 20151026144139) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
