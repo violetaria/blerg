08a4c0502b39c19e8912f4b58b61d6f19b9588a4
Brit Butler <redline6561@gmail.com>
2015-10-26 11:03:20 -0400

    Add format attribute to Post models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026144139) do
+ActiveRecord::Schema.define(version: 20151026150244) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151026144139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.string   "format"
   end
 
   create_table "tags", force: :cascade do |t|
