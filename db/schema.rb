1bc7c8b1e28c1e4e0d0e36bf0b54d5b323e92af5
Brit Butler <redline6561@gmail.com>
2015-10-26 10:36:25 -0400

    Add a post model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026142522) do
+ActiveRecord::Schema.define(version: 20151026143420) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "written_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
