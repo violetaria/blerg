cfe049861c1511c853c18c02c0f4267fb6993727
violetaria <terri.chu@outlook.com>
2015-11-22 10:20:09 -0500

    updated tags controller to use a ! in the AR call

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
-    @tag = Tag.find_by(name: params[:name])
+    @tag = Tag.find_by!(name: params[:name])  #will return a record not found if called with ! when data is not found
     @posts = @tag.posts
     #@tag_name = params[:name]
     render :show
